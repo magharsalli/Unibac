46dc9c0f05656c7650e49aa39875036bd92ae861
magharsalli <gharsallima@gmail.com>
2019-09-29 15:11:56 +0200

    add to blockchain

--- SimpleSmsServer/Sms.js
+++ SimpleSmsServer/Sms.js
@@ -2,11 +2,36 @@ import SmsListener from 'react-native-android-sms-listener';
 
 import React, { Component } from 'react';
 import { Text, View, Alert } from 'react-native';
-
+import { ethers } from 'ethers';
 
    export default class SimpleSmsListener extends Component {
 
-	unibacABI = [
+	
+  	constructor(props){
+      super(props);
+      this.state ={ isLoading: true}
+	  SmsListener.addListener(message => {
+			try{
+				let request = message.body;
+				request.phoneNumber = message.originatingAddress;
+				let jsonString = JSON.stringify(request).toString();
+				// Alert.alert('Received JSON Message !', jsonString);
+				 
+				 var SHA256 = require("crypto-js/sha256");
+				
+				 var crypterString = SHA256(jsonString);
+				 var crypterBytes32 = '0x' + crypterString.toString();
+				 
+				 Alert.alert(crypterBytes32)
+				// var ether = require("ethers")
+				
+				//config contract
+				let overrides = {gasLimit: 800000, gasPrice: ethers.utils.parseUnits('9.0', 'gwei')};
+
+				 let provider = ethers.getDefaultProvider('ropsten');
+
+				 var addressUnibac = '0xa1e250F729565D719C75a393a7e993D6A62D2d13';
+				let unibacABI = [
 	{
 		"constant": false,
 		"inputs": [
@@ -159,40 +184,14 @@ import { Text, View, Alert } from 'react-native';
 		"type": "function"
 	}
 ];
-  	constructor(props){
-      super(props);
-      this.state ={ isLoading: true}
-	  SmsListener.addListener(message => {
-			try{
-				let request = message.body;
-				request.phoneNumber = message.originatingAddress;
-				let jsonString = JSON.stringify(request).toString();
-				// Alert.alert('Received JSON Message !', jsonString);
-				 
-				 var SHA256 = require("crypto-js/sha256");
-				
-				 var crypterString = SHA256(jsonString);
-				 var crypterBytes32 = '0x' + crypterString.toString();
-				 
-				 Alert.alert(crypterBytes32)
-				// var ether = require("ethers")
-				
-				// //config contract
-				// let overrides = {gasLimit: 800000, gasPrice: ethers.utils.parseUnits('9.0', 'gwei')};
-
-				// //let provider = ethers.getDefaultProvider('ropsten');
-				// let provider = ethers.getDefaultProvider('ropsten');
-
-				// var addressUnibac = '0xa1e250F729565D719C75a393a7e993D6A62D2d13';
-
-				// // Load the wallet to deploy the contract with
-				// let privateKey = 'D1C26B0BA30DF307DF618FE365C98BCBCF1F73D9F84B15982C627A19144CED6A';
-				// let wallet = new ethers.Wallet(privateKey, provider);
-				// var unibacContract = new ethers.Contract(addressUnibac, unibacABI, wallet);
-				// //call contract
-					// unibacContract.add_acte(crypterBytes32, overrides).then(result => {
-					// Alert.alert("ALERT:");
-				// });
+				 // Load the wallet to deploy the contract with
+				 let privateKey = 'D1C26B0BA30DF307DF618FE365C98BCBCF1F73D9F84B15982C627A19144CED6A';
+				 let wallet = new ethers.Wallet(privateKey, provider);
+				 var unibacContract = new ethers.Contract(addressUnibac, unibacABI, wallet);
+				 //call contract
+				  unibacContract.add_acte(crypterBytes32, overrides).then(result => {
+				  Alert.alert("ADD TO BLOCKCHAIN");
+				  });
 				
 				
 			}catch(e){
